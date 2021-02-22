d7f3fe53a8edd288fdb48175e371d436e5f5ac93
slingfox <slingfox@users.noreply.github.com>
2021-02-22 21:34:45 +0100

    Parse payload

--- handle-comment.js
+++ handle-comment.js
@@ -1,9 +1,44 @@
 const fs = require('fs');
 
-
-
-console.log(process.env);
+// Constants
+const PERMITTED_AUTHOR = 'slingfox';
+const COMMAND_PATCH = '!patch';
+const COMMAND_MINOR = '!minor';
+const COMMAND_MAJOR = '!major';
 const FILE_PATH = 'openapi.json';
+
+
+// Check that ist was a comment
+if (process.env.GITHUB_EVENT_NAME !== 'issue_comment') {
+  console.warn('Not an issue comment');
+  return;
+}
+
+// Check that the user is permitted to execute via comment
+if (process.env.GITHUB_ACTOR !== PERMITTED_AUTHOR) {
+  console.warn('Not a comment by a permitted author');
+  return;
+}
+
+// Read issue payload
+const payload = JSON.parse(fs.readFileSync(process.env.GITHUB_EVENT_PATH, { encoding: 'utf8' }));
+
+console.log(payload);
+
+// Parse the comment to find a bump command
+const bumpAt = 2; // which part of the version triplet to bump
+if (payload.comment.body.contains(COMMAND_PATCH)) {
+  bumpAt = 2;
+} else if (payload.comment.body.contains(COMMAND_MINOR)) {
+  bumpAt = 1;
+} else if (payload.comment.body.contains(COMMAND_MAJOR)) {
+  bumpAt = 0;
+} else {
+  console.warn('No command found in: ' + payload.comment.body);
+  return;
+}
+
+console.log('Bump: ' + bumpAt);
 
 const data = fs.readFileSync(FILE_PATH);
 const json = JSON.parse(data);
@@ -13,7 +48,6 @@ const oldVersionStr = json.info.version;
 const oldVersion = oldVersionStr.match(/\d+/g);
 console.log(oldVersion);
 
-const bumpAt = 2;
 
 // https://github.com/weflex/semver-increment/blob/master/index.js
 let newVersion = []
